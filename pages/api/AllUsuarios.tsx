f03f10a9dbb1ba352db6314c34f6450ea9101fd8
Sergio Junior <sergiojunio@NATALCENTER.LOCAL>
2023-07-18 14:35:02 -0300

    Rota de usuários passando a enviar sobrenome

--- pages/api/AllUsuarios.tsx
+++ pages/api/AllUsuarios.tsx
@@ -7,8 +7,17 @@ export default async function handler(
 ) {
   try {
     const result = await sql`SELECT * FROM usuario ;`;
+    const usuarios = result.rows;
+    
+    usuarios.map( (item) => { 
+      const nomeDoUsuário = item.nome;
+      const nome = nomeDoUsuário.split(' ')[0];
+      const sobrenome = nomeDoUsuário.split(' ')[1];
+      item.nome = nome;
+      item.sobrenome = sobrenome;
+    })
 
-    return response.status(200).json( result.rows );
+    return response.status(200).json( usuarios );
   } catch (error) {
     return response.status(500).json({ error });
   }
