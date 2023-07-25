89af15e073f242b3737f65beaf5c51db537d5d4e
JrSergio00 <junior.sergio@escolar.ifrn.edu.br>
2023-07-25 19:47:19 -0300

    Rota de edição de usuários

--- pages/api/EditUsuario.tsx
+++ pages/api/EditUsuario.tsx
@@ -1,20 +1,50 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { sql } from '@vercel/postgres';
+import cors from 'cors';
+
+// // Criar o middleware do cors
+// const corsMiddleware = cors({
+//   origin: 'http://localhost:5173', // Substitua isso pelo endereço do seu frontend
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+// });
+
+// // Aplicar o middleware do cors apenas para essa rota
+// export const config = {
+//   api: {
+//     bodyParser: false, // Desabilita o bodyParser padrão
+//   },
+// };
 
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+
   try {
+    // Utilize o corsMiddleware como middleware antes de executar sua API
+    // await new Promise<void>((resolve, reject) => {
+    //   corsMiddleware(request, response, (err?: Error) => {
+    //     if (err) {
+    //       return reject(err);
+    //     }
+    //     resolve();
+    //   });
+    // });
+
     const { id }: any = request.query;
+    const { nome, sobrenome, contato } = request.body;
+    const nomeCompleto = nome + " " + sobrenome;
+
     const result = await sql`
-      SELECT 
-        * 
-      FROM 
-        usuario
-      WHERE
-        id = ${id}
-      ;`;
+    UPDATE 
+      usuario
+    SET 
+      nome = ${nomeCompleto},
+      contato = ${contato}
+    WHERE 
+      id =  ${id}
+    ;`;
 
     // result.rows.map((item: any) => {
     // });
