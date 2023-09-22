de75ff8fb99996e3639234ff38362314ea8b250e
JrSergio00 <junior.sergio@escolar.ifrn.edu.br>
2023-09-22 20:28:39 -0300

    Erro de cors

--- pages/api/GetVerifyNumber.tsx
+++ pages/api/GetVerifyNumber.tsx
@@ -1,26 +1,36 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { sql } from '@vercel/postgres';
- 
+import cors from 'cors';
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Defina a origem permitida específica
+  methods: ['POST'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+};
+
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-  try {
-    const { number } = request.body;
-    // const { id }: any = request.query;
+  await cors(corsOptions)(request, response, async () => {
+    try {
+      const { number } = request.body;
+      // const { id }: any = request.query;
 
-    const result = await sql`
-      SELECT 
-        usuario.id,
-        usuario.nome
-      FROM
-        usuario
-      WHERE 
-        usuario.contato =  ${number}
-    ;`;
+      const result = await sql`
+        SELECT 
+          usuario.id,
+          usuario.nome
+        FROM
+          usuario
+        WHERE 
+          usuario.contato =  ${number}
+      ;`;
 
-    return response.status(200).json( result.rows );
-  } catch (error) {
-    return response.status(500).json({ error });
-  }
+      return response.status(200).json(result.rows);
+    } 
+    catch (error) {
+      return response.status(500).json({ error });
+    }
+  })
 }
